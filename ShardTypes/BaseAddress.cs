6427e8d55519eb57907ab3199a7349d783ef8b8d
Georg <georg@esnwt.org>
2025-12-29 00:27:00 +0100

    Cleaned up repository Removed original card set Removed original BaseAddress Removed build output directories Removed IDE config files Added gitignore

--- ShardTypes/BaseAddress.cs
+++ ShardTypes/BaseAddress.cs
@@ -2,5 +2,10 @@
 
 public static class BaseAddress
 {
-    public static string Address => "https://api.belada.at";
+    public static string Address => 
+        #if DEBUG
+                "http://localhost:5039";
+        #else
+                "https://api.example.com";  // TODO: REPLACE WITH YOUR PRODUCTION URL
+        #endif
 }
---
+++ ShardTypes/CardsTemplate.cs
@@ -0,0 +1,28 @@
+﻿// TODO: COPY THIS FILE AS Cards.cs AND ADD YOUR CARDS
+
+namespace ShardTypes;
+
+//Available card types: CardType.Action, CardType.Trap, CardType.Effect
+public static class Cards
+{
+    public static List<Card> AvailableCards = new()
+    {
+        new Card()
+        {
+            Name = "Name of Card 1",
+            Description = "Everyone drinks",
+            NumberOfDrinks = 1,
+            Type = CardType.Action,
+            ImageName = "card1.png",
+        },
+        new Card()
+        {
+            Name = "Second Card Title",
+            Description = "You drink",
+            NumberOfDrinks = 1,
+            Type = CardType.Action,
+            ImageName = "card2.png",
+        },
+        // Add more cards here
+    };
+}
